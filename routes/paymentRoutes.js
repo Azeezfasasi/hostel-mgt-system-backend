747a0306d844a4304f00d7fa18924579de79f7fc
Azeezfasasi <fas.azeez@gmail.com>
2025-09-14 10:29:32 +0100

    corrected

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const { initiatePayment, confirmPayment, getPayments, initCredoPayment, verifyCredoPayment }  = require('../controllers/paymentController.js');
+const { initCredoPayment, confirmPayment, getPayments, initCredoPayment, verifyCredoPayment }  = require('../controllers/paymentController.js');
 // POST - /api/payment/init (Credo direct API)
 router.post("/init", initCredoPayment);
 
 // GET - /api/payment/verify (Credo direct API)
 router.get("/verify", verifyCredoPayment);
-
-const router = express.Router();
 
 // POST - /api/payment
 router.post("/", initiatePayment);
