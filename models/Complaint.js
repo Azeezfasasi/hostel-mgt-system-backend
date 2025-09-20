603c69e343dcb6b172786a2d5eba72315a8f83ab
Azeezfasasi <fas.azeez@gmail.com>
2025-09-20 13:59:12 +0100

    updated comp

--- models/Complaint.js
+++ models/Complaint.js
@@ -4,7 +4,7 @@ const complaintSchema = new mongoose.Schema({
   student: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   category: { type: String, enum: ["maintenance", "electricity", "security", "other"], required: true },
   description: { type: String, required: true },
-  status: { type: String, enum: ["open", "in-progress", "resolved"], default: "open" },
+  status: { type: String, enum: ["open", "in-progress", "resolved", "closed", "pending"], default: "open" },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, // staff/admin
 }, { timestamps: true });
 
