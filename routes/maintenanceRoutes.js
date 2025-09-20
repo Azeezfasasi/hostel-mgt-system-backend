46b84a2b787941fc6f5d12a4da2280f3484b43a1
Azeezfasasi <fas.azeez@gmail.com>
2025-09-20 16:13:38 +0100

    checks

--- routes/maintenanceRoutes.js
+++ routes/maintenanceRoutes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const { createMaintenanceRequest, getMaintenanceRequests, updateMaintenanceStatus, getMaintenanceRequest, deleteMaintenanceRequest }  = require('../controllers/maintenanceController');
+const { auth, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
 
 // POST /api/maintenance
-router.post("/", createMaintenanceRequest);
+router.post("/", auth, createMaintenanceRequest);
 
 // GET /api/maintenance
-router.get("/", getMaintenanceRequests);
+router.get("/", auth, authorizeRoles, getMaintenanceRequests);
 
 // GET /api/maintenance/:id
-router.get("/:id", getMaintenanceRequest);
+router.get("/:id", auth, authorizeRoles, getMaintenanceRequest);
 
 // PUT /api/maintenance/:id
-router.put("/:id", updateMaintenanceStatus);
+router.put("/:id", auth, authorizeRoles, updateMaintenanceStatus);
 
 // PATCH /api/maintenance/:id
-router.patch("/:id", updateMaintenanceStatus);
+router.patch("/:id", auth, authorizeRoles, updateMaintenanceStatus);
 
 // DELETE /api/maintenance/:id
-router.delete("/:id", deleteMaintenanceRequest);
+router.delete("/:id", auth, authorizeRoles, deleteMaintenanceRequest);
 
 module.exports = router;
